055ff651a059adaac8129b4d0315a08f985d0dff
Crystinum <mattlee525@gmail.com>
2021-02-11 18:41:20 -0800

    Creating findInd and expansion functions

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include <vector>
 using namespace std;
 
+
+
 class Node {
 public: 
 	Node(vector<vector<int>> state, int size);
@@ -17,6 +19,9 @@ public:
 	vector<vector<int>> state;
 	int sz;
 	void print();
+	vector<int> findInd(vector<vector<int>> state, int num);
+	void expand(Node head);
+	
 };
 
 int main() {
@@ -38,6 +43,7 @@ int main() {
 	test.print();
 
 	cout << "-------------after node print------------------" << endl;
+	
 	
 
 }
@@ -86,6 +92,46 @@ Node::Node(Node* prev, Node* ex1, Node* ex2, Node* ex3, Node* ex4, int state[][]
 	sz = size;
 }
 */
+void Node::expand(Node head) {
+	vector<int> indmove = findInd(head.state, 0);				// index of "0"
+
+	int posmove = 4;										// possible moves
+
+	if (indmove.at(0) == 0 || indmove.at(0) == indmove.size() - 1) {
+		posmove--;
+	}
+	if (indmove.at(1) == 0 || indmove.at(1) == indmove.size() - 1) {
+		posmove--;
+	}
+
+	if (posmove == 4) {
+		
+	}
+	else if (posmove == 3) {
+
+	}
+	else if (posmove == 2) {
+
+	}
+	else if (posmove > 4 || posmove < 2) {
+		cout << "Error: Invalid number of possible moves. Expansion failed." << endl;
+	}
+
+}
+
+vector<int> Node::findInd(vector<vector<int>> state, int num) {
+	vector<int> index;
+	for (int i = 0; i < state.size(); i++) {
+		for (int j = 0; j < state.size(); j++) {
+			if (state[i][j] == num) {
+				index.push_back(i);
+				index.push_back(j);
+			}
+		}
+	}
+	return index;
+}
+
 void Node::print() {
 	for (int i = 0; i < sz; i++) {
 		for (int j = 0; j < sz; j++) {
